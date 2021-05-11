fccc3a78b094f0016032238c39dcae7390487b76
moshiur rahman <moshiurrahman888137@gmail.com>
2021-05-11 09:40:23 +0600

    Added a new mathood called display

--- hello.java
+++ hello.java
@@ -1,5 +1,11 @@
 public class hello{
     public static void main(String[]args){
         System.out.printIn('Vs Code is Awesome...');
+
+        greeting("moshiur");
+    }
+
+    public static void greeting(String name){
+        System.out.printIn("welcome " + name);
     }
 }
