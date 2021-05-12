1ab75ad912303dfac41524f4c6eb070969d18575
moshiur rahman <moshiurrahman888137@gmail.com>
2021-05-12 07:55:23 +0600

    change the text

--- hello.java
+++ hello.java
@@ -1,5 +1,12 @@
 public class hello{
     public static void main(String[]args){
         System.out.printIn('Vs Code is Awesome...');
+ 
+        greeting("Git");
+    }
+
+
+    public static void greeting(String name){
+        System.out.printIn("Bay Bay " + name)
     }
 }
